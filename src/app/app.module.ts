15f0a17e929d84295f9bb2789575037e86649193
aejunk <aejunk@aejunks-MacBook-Pro.local>
2019-08-15 03:16:37 +0700

    modified src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,38 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { RegisterPage } from '../pages/register/register';
+import { PromotionPage } from '../pages/promotion/promotion';
+import { BentoPage } from '../pages/bento/bento';
+import { SnackPage } from '../pages/snack/snack';
+import { RicePage } from '../pages/rice/rice';
+import { SushiPage } from '../pages/sushi/sushi';
+import { DelicatessenPage } from '../pages/delicatessen/delicatessen';
+import { BeveragePage } from '../pages/beverage/beverage';
+import { ProductDetailPage } from '../pages/product-detail/product-detail';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+// My Header
+import { MyHeaderComponent } from '../components/my-header/my-header'
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    RegisterPage,
+    PromotionPage,
+    BentoPage,
+    SnackPage,
+    RicePage,
+    SushiPage,
+    DelicatessenPage,
+    BeveragePage,
+    ProductDetailPage,
+    // My Header
+    MyHeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +46,16 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    RegisterPage,
+    PromotionPage,
+    BentoPage,
+    SnackPage,
+    RicePage,
+    SushiPage,
+    DelicatessenPage,
+    BeveragePage,
+    ProductDetailPage
   ],
   providers: [
     StatusBar,
