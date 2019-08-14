0c14e784f8d4f28df227ddb6e89c0c7d02139714
aejunk <aejunk@aejunks-MacBook-Pro.local>
2019-08-15 03:15:52 +0700

    modified src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { RegisterPage } from '../pages/register/register';
+import { PromotionPage } from '../pages/promotion/promotion';
+import { BentoPage } from '../pages/bento/bento';
+import { SnackPage } from '../pages/snack/snack';
+import { RicePage } from '../pages/rice/rice';
+import { SushiPage } from '../pages/sushi/sushi';
+import { DelicatessenPage } from '../pages/delicatessen/delicatessen';
+import { BeveragePage } from '../pages/beverage/beverage';
+import { ProductDetailPage } from '../pages/product-detail/product-detail';
 
 @Component({
   templateUrl: 'app.html'
@@ -12,7 +21,8 @@ import { ListPage } from '../pages/list/list';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  // rootPage: any = HomePage;
+  rootPage: any = PromotionPage;
 
   pages: Array<{title: string, component: any}>;
 
@@ -21,8 +31,16 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      // { title: 'Home', component: HomePage },
+      // { title: 'List', component: ListPage },
+      { title: 'เข้าสู่ระบบ | สมัครสมาชิก', component: RegisterPage },
+      { title: 'โปรโมชั่น', component: PromotionPage },
+      { title: 'ชุดเบนโตะ', component: BentoPage },
+      { title: 'อาหารทานเล่น', component: SnackPage },
+      { title: 'ข้าวหน้าและซุปต่าง ๆ', component: RicePage },
+      { title: 'ซูชิ', component: SushiPage },
+      { title: 'อาหารสำเร็จรูป', component: DelicatessenPage },
+      { title: 'เครื่องดื่ม', component: BeveragePage }
     ];
 
   }
